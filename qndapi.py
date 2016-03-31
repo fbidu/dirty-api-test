319db5efb4aad46d9ed9ed8111fa12d2784f2abd
Felipe Rodrigues <felipe@felipevr.com>
2016-03-31 19:32:20 -0300

    Cleaning and refactoring the main file

--- qndapi.py
+++ qndapi.py
@@ -7,10 +7,19 @@ import json
 from datetime import datetime
 from model import Log
 
-ERROR_MAIL = """
+ASSERTION_ERROR_MAIL = """
 <p>Hello,</br>
 The amazing Quick 'n' Dirty API tester failed to assert that the path</br>
-<b>{path}</b> returned a HTTP-200 code.</br>
+<b>{path}</b> of the API {api} returned a HTTP-200 code.</br>
+The test was performed at {datetime}</br></p>
+<p>Best of luck,</br>
+QNDAPIT<p>
+"""
+
+CONNECTION_ERROR_MAIL = """
+<p>Hello,</br>
+The amazing Quick 'n' Dirty API tester failed to connect to</br>
+<b>{path}</b> of the API {api}.</br>
 The test was performed at {datetime}</br></p>
 <p>Best of luck,</br>
 QNDAPIT<p>
@@ -18,51 +27,70 @@ QNDAPIT<p>
 
 def main():
     try:
+        # Loading the settings
         current_dir = path.dirname(__file__)
         config_path = path.join(current_dir, 'config.yaml')
         config = yaml.load(open(config_path).read())
 
         apis = config['APIs']
 
+        # For each API
         for api in apis.keys():
-            endpoints = [str(api) + str(endpoint)
-                         for endpoint in list(apis[api])]
+            # Load the endpoints
+            api = str(api)
+            endpoints = list(apis[api])
 
-        for endpoint in endpoints:
-            try:
-                request = requests.get(endpoint, timeout=2)
-            except:
-                Log(line='Connection error', api=api, endpoint=endpoint,
-                    timestamp=datetime.now()).save()
-            else:
+            # For each endpoint
+            for endpoint in endpoints:
+                endpoint = str(endpoint)
+                url = api + endpoint
+                now = datetime.now()
                 try:
-                    assert request.status_code == 200
-                except AssertionError:
-                    Log(line='Error!', api=api, endpoint=endpoint,
-                        timestamp=datetime.now()).save()
-                    now = datetime.now().strftime("%d/%m/%Y @ %Hh%mmin")
-                    message = ERROR_MAIL.format(path=endpoint, datetime=now)
-                    subject = "QNDAPIT Error!"
-                    receiver = str(config['email']['receiver'])
-                    driver = str(config['email']['driver'])
-
-                    if driver == 'devnup-email':
-                        api_data = {
-                            'token': str(config['email']['token']),
-                            'message': {
-                                'to': receiver,
-                                'subject': subject,
-                                'body': message
-                            }
-                        }
-                        requests.post('http://email.devnup.com/api/gateway/send',
-                                      data=json.dumps(api_data))
+                    # We try to connect
+                    request = requests.get(url, timeout=5)
+                except:
+                    # If we fail...
+                    Log(line='Connection error', api=api, endpoint=endpoint,
+                        timestamp=now).save()
                 else:
-                    Log(line='Success', api=api,
-                        endpoint=endpoint, timestamp=datetime.now()).save()
+                    # If we succeed.
+                    try:
+                        # We try to assert that the response was 200
+                        assert request.status_code == 200
+                    except AssertionError:
+                        # If we fail...
+                        Log(line='HTTP Error', api=api, endpoint=endpoint,
+                            timestamp=datetime.now()).save()
+
+                        timestamp = now.strftime("%d/%m/%Y @ %Hh%mmin")
+                        message = ASSERTION_ERROR_MAIL.format(path=endpoint,
+                                                    datetime=timestamp,
+                                                    api=api)
+
+                        subject = "QNDAPIT Error!"
+                        receiver = str(config['email']['receiver'])
+                        token = str(config['email']['token'])
+
+                        send_assertion_error_email(token, receiver,
+                                                   subject, message)
+                    else:
+                        # If we do assert that the HTTP code was 200
+                        Log(line='Success', api=api,
+                            endpoint=endpoint, timestamp=now).save()
     except:
+        # If we fail to load the config file
         raise
 
+def send_assertion_error_email(token, receiver, subject, message):
+    api_data = {
+        'token': token,
+        'message': {
+            'to': receiver,
+            'subject': subject,
+            'body': message
+        }
+    }
+    requests.post('http://email.devnup.com/api/gateway/send', json=api_data)
 
 if __name__ == "__main__":
     main()
