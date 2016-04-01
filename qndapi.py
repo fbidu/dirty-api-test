897ee6d1c8bebc74127e30b2c7051e65561a7291
Felipe Rodrigues <felipe@felipevr.com>
2016-03-31 21:38:41 -0300

    Extracting some functions

--- qndapi.py
+++ qndapi.py
@@ -43,43 +43,47 @@ def main():
             # For each endpoint
             for endpoint in endpoints:
                 endpoint = str(endpoint)
-                url = api + endpoint
-                now = datetime.now()
-                try:
-                    # We try to connect
-                    request = requests.get(url, timeout=5)
-                except:
-                    # If we fail...
-                    Log(line='Connection error', api=api, endpoint=endpoint,
-                        timestamp=now).save()
-                else:
-                    # If we succeed.
-                    try:
-                        # We try to assert that the response was 200
-                        assert request.status_code == 200
-                    except AssertionError:
-                        # If we fail...
-                        Log(line='HTTP Error', api=api, endpoint=endpoint,
-                            timestamp=datetime.now()).save()
+                test_endpoint(api, endpoint, config)
 
-                        timestamp = now.strftime("%d/%m/%Y @ %Hh%mmin")
-                        message = ASSERTION_ERROR_MAIL.format(path=endpoint,
-                                                    datetime=timestamp,
-                                                    api=api)
-
-                        subject = "QNDAPIT Error!"
-                        receiver = str(config['email']['receiver'])
-                        token = str(config['email']['token'])
-
-                        send_assertion_error_email(token, receiver,
-                                                   subject, message)
-                    else:
-                        # If we do assert that the HTTP code was 200
-                        Log(line='Success', api=api,
-                            endpoint=endpoint, timestamp=now).save()
+    # If we fail to load the config file
     except:
-        # If we fail to load the config file
         raise
+
+
+def test_endpoint(api, endpoint, config, expected_code=200):
+    url = api + endpoint
+    now = datetime.now()
+    try:
+        # We try to connect
+        request = requests.get(url, timeout=5)
+    except:
+        # If we fail...
+        Log(line='Connection error', api=api, endpoint=endpoint,
+            timestamp=now).save()
+    else:
+        # If we succeed.
+        try:
+            # We try to assert that the response was 200
+            assert request.status_code == 200
+        except AssertionError:
+            # If we fail...
+            Log(line='HTTP Error', api=api, endpoint=endpoint,
+                timestamp=datetime.now()).save()
+
+            timestamp = now.strftime("%d/%m/%Y @ %Hh%mmin")
+            message = ASSERTION_ERROR_MAIL.format(path=endpoint,
+                                                  datetime=timestamp, api=api)
+
+            subject = "QNDAPIT Error!"
+            receiver = str(config['email']['receiver'])
+            token = str(config['email']['token'])
+
+            send_assertion_error_email(token, receiver, subject, message)
+        else:
+            # If we do assert that the HTTP code was 200
+            Log(line='Success', api=api, endpoint=endpoint,
+                timestamp=now).save()
+
 
 def send_assertion_error_email(token, receiver, subject, message):
     api_data = {
@@ -87,7 +91,7 @@ def send_assertion_error_email(token, receiver, subject, message):
         'message': {
             'to': receiver,
             'subject': subject,
-            'body': message
+            'html': message
         }
     }
     requests.post('http://email.devnup.com/api/gateway/send', json=api_data)
